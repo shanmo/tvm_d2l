324e18254a098c64e0696e2861af9ec865ab80b9
uie85879 <mo.shan@continental-corporation.com>
2022-06-18 19:08:27 +0800

    add

---
+++ cpu/utils.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+def get_abc(shape, constructor=None):
+    """Return random a, b and empty c with the same shape.
+    """
+    np.random.seed(0)
+    a = np.random.normal(size=shape).astype(np.float32)
+    b = np.random.normal(size=shape).astype(np.float32)
+    c = np.empty_like(a)
+    if constructor:
+        a, b, c = [constructor(x) for x in (a, b, c)]
+    return a, b, c
