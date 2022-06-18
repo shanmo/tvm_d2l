c712b4afb407160f383eade71d089e9476efb30b
uie85879 <mo.shan@continental-corporation.com>
2022-06-18 21:30:08 +0800

    add

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # tvm_d2l
 
 - this is the repo for [Dive into Deep Learning Compiler](https://tvm.d2l.ai/)
-- platform is NUC11 
+- platform is NUC11, details 
+```
+(base) sean@nuc:~/workspace$ gcc -march=native -Q --help=target|grep march
+  -march=                     		tigerlake
+  Known valid arguments for -march= option:
+```
+    - `tigerlake` can be used in tvm for `mcpu` 
 
 # references 
 
--- cpu/utils.py
+++ cpu/utils.py
@@ -10,3 +10,19 @@ def get_abc(shape, constructor=None):
     if constructor:
         a, b, c = [constructor(x) for x in (a, b, c)]
     return a, b, c
+
+def get_bcast_data(shape1, shape2, constructor=None):
+    """Return random tensors a, b
+    and empty tensor c to store broadcast results between a and b
+    shape1, shape2: shapes of input tensors
+    constructor : user-defined tensor constructor
+    """
+    np.random.seed(0)
+    a = np.random.normal(size=shape1).astype("float32")
+    b = np.random.normal(size=shape2).astype("float32")
+    out_shape = (shape1[0] if shape2[0] == 1 else shape2[0],
+                 shape1[1] if shape2[1] == 1 else shape2[1])
+    c = np.empty(out_shape, dtype='float32')
+    if constructor:
+        a, b, c = [constructor(x) for x in (a, b, c)]
+    return a, b, c
