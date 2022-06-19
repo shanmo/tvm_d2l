b1dda4d997bacb31c65600f9a6798a7c05d3ca78
uie85879 <mo.shan@continental-corporation.com>
2022-06-19 16:47:44 +0800

    add

--- cpu/utils.py
+++ cpu/utils.py
@@ -1,4 +1,10 @@
 import numpy as np 
+import mxnet as mx
+from tvm import te
+
+def conv_mxnet(data, weight, bias, out, k, p, s):
+    mx.nd.Convolution(data, weight, bias, kernel=(k,k), stride=(s,s),
+                      pad=(p,p), num_filter=out.shape[1], out=out)
 
 def get_abc(shape, constructor=None):
     """Return random a, b and empty c with the same shape.
@@ -26,3 +32,156 @@ def get_bcast_data(shape1, shape2, constructor=None):
     if constructor:
         a, b, c = [constructor(x) for x in (a, b, c)]
     return a, b, c
+
+def conv_out_size(n, k, p, s):
+    """Compute the output size by given input size n (width or height),
+    kernel size k, padding p, and stride s
+    Return output size (width or height)
+    """
+    return (n - k + 2 * p)//s + 1
+
+def get_conv_data(oc, ic, n, k, p=0, s=1, constructor=None, conv_type='direct'):
+    """Return random 3-D data tensor, 3-D kernel tenor and empty 3-D output
+    tensor with the shapes specified by input arguments.
+    oc, ic : output and input channels
+    n : input width and height
+    k : kernel width and height
+    p : padding size, default 0
+    s : stride, default 1
+    conv_type: either direct 2D or depthwise, default direct
+    constructor : user-defined tensor constructor
+    """
+    np.random.seed(0)
+    data = np.random.normal(size=(ic, n, n)).astype('float32')
+    ic_weight = ic
+    if conv_type == 'depthwise':
+        ic_weight = 1
+    weight = np.random.normal(size=(oc, ic_weight, k, k)).astype('float32')
+    on = conv_out_size(n, k, p, s)
+    out = np.empty((oc, on, on), dtype='float32')
+    if constructor:
+        data, weight, out = (constructor(x) for x in [data, weight, out])
+    return data, weight, out
+
+def get_conv_data_mxnet(oc, ic, n, k, p, s, ctx='cpu', conv_type='direct'):
+    ctx = getattr(mx, ctx)()
+    data, weight, out = get_conv_data(oc, ic, n, k, p, s,
+                                      constructor=lambda x: mx.nd.array(x, ctx=ctx),
+                                      conv_type=conv_type)
+    data, out = data.expand_dims(axis=0), out.expand_dims(axis=0)
+    bias = mx.nd.zeros(out.shape[1], ctx=ctx)
+    return data, weight, bias, out
+
+def padding(X, ph, pw, val=0):
+    """Pad X with the given value in 2-D
+    ph, pw : height and width padding
+    val : padding value, default 0
+    """
+    assert len(X.shape) >= 2
+    nh, nw = X.shape[-2], X.shape[-1]
+    return te.compute(
+            (*X.shape[0:-2], nh+ph*2, nw+pw*2),
+            lambda *i: te.if_then_else(
+                te.any(i[-2]<ph, i[-2]>=nh+ph, i[-1]<pw, i[-1]>=nw+pw),
+                val, X[i[:-2]+(i[-2]-ph, i[-1]-pw)]),
+            name='PaddedX')
+
+def conv(oc, ic, nh, nw, kh, kw, ph=0, pw=0, sh=1, sw=1):
+    """Convolution
+    oc, ic : output and input channels
+    nh, nw : input width and height
+    kh, kw : kernel width and height
+    ph, pw : height and width padding sizes, default 0
+    sh, sw : height and width strides, default 1
+    """
+    # reduction axes
+    ric = te.reduce_axis((0, ic), name='ric')
+    rkh = te.reduce_axis((0, kh), name='rkh')
+    rkw = te.reduce_axis((0, kw), name='rkw')
+    # output height and weights
+    oh = conv_out_size(nh, kh, ph, sh)
+    ow = conv_out_size(nw, kw, pw, sw)
+    # pad X and then compute Y
+    X = te.placeholder((ic, nh, nw), name='X')
+    K = te.placeholder((oc, ic, kh, kw), name='K')
+    PaddedX = padding(X, ph, pw) if ph * pw != 0 else X
+    Y = te.compute(
+        (oc, oh, ow),
+        lambda c, i, j: te.sum(
+            PaddedX[ric, i*sh+rkh, j*sw+rkw] * K[c, ric, rkh, rkw],
+            axis=[ric, rkh, rkw]), name='Y')
+    return X, K, Y, PaddedX
+
+from IPython import display
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Defined in file: ./chapter_cpu_schedules/vector_add.md
+def plot(X, Y, xlabel=None, ylabel=None, legend=[], xlim=None,
+         ylim=None, xscale='linear', yscale='linear', fmts=None,
+         figsize=(4.5, 3)):
+    """Plot multiple lines"""
+    display.set_matplotlib_formats('svg')
+    plt.rcParams['figure.figsize'] = figsize
+    axes = plt.gca()
+    X, Y = np.array(X), np.array(Y)
+    if X.shape != Y.shape: X = [X] * len(Y)
+    if not fmts: fmts = ['-'] * len(X)
+    for x, y, fmt in zip(X, Y, fmts):
+        axes.plot(x, y, fmt)
+    axes.set_xlabel(xlabel)
+    axes.set_ylabel(ylabel)
+    axes.set_xscale(xscale)
+    axes.set_yscale(yscale)
+    axes.set_xlim(xlim)
+    axes.set_ylim(ylim)
+    if legend: axes.legend(legend)
+    axes.grid()
+
+
+# Defined in file: ./chapter_cpu_schedules/vector_add.md
+def plot_gflops(sizes, gflops, legend, xlabel='Size'):
+    plot(sizes, gflops, xlabel=xlabel, ylabel='GFLOPS',
+             xscale='log', yscale='log',
+             legend=legend, fmts=['--']*(len(gflops)-1)+['-'])
+
+
+
+
+def bench_workload(workload):
+    """Benchmark a workload
+    workload: a method that accept a num_repeat argument
+    and return its total execution time
+    """
+    workload(1)  # warmup
+    time = workload(1)  # the time to run once
+    if time > 1: return time
+    # The number of repeats to measure at least 1 second
+    num_repeats = max(int(1.0 / time), 5)
+    return workload(num_repeats) / num_repeats
+
+def conv_gflop(oc, ic, n, k, p, s):
+    """Compute the #floating point operations in a convolution.
+    The arguments are output channels oc, input channels ic, input size n,
+    kernel size k, padding p and stride s.
+    """
+    on = conv_out_size(n, k, p, s)
+    return 2 * oc * ic * on * on * k * k / 1e9
+
+import tvm 
+
+def bench_conv_tvm(func, sizes, target):
+    def workload(nrepeats):
+        dev = tvm.device(target)
+        timer = mod.time_evaluator(mod.entry_name, dev=dev, number=nrepeats)
+        return timer(x, k, y).mean * nrepeats
+    gflops, times = [], []
+    for (c, n, k) in sizes:
+        args = c, c, n, k, (k-1)//2, 1 # oc, ic, n, k, p, s
+        s, (X, K, Y) = func(*args)
+        mod = tvm.build(s, [X, K, Y], target)
+        x, k, y = get_conv_data(
+            *args, lambda x: tvm.nd.array(x))
+        times.append(bench_workload(workload))
+        gflops.append(conv_gflop(*args))
+    return np.array(gflops) / np.array(times)
